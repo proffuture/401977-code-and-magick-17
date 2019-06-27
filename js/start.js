5fe34f9e6c22243912602f680b165a9f5d4783e3
Sergey Petrov <newproffdesign@yandex.ru>
2019-06-27 22:32:34 +0500

    пор

---
+++ js/start.js
@@ -0,0 +1,8 @@
+window.renderStatistics = function (ctx, names, times) {
+  var i = 0;
+  for(Math.random(), i < names.length, i++) {
+
+  }
+};
+
+renderStatistics (['Лёва', 'Филимон', 'Сергей', 'Ромка']);
